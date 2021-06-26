ba18655db4c42bd123058017bd88f6fa08e88243
Bo Lin <coinmini@gmail.com>
2021-06-26 20:50:22 +0800

    014

--- src/multiexp.rs
+++ src/multiexp.rs
@@ -283,6 +283,32 @@ where
         })
 }
 
+// Added by jackoelv for C2 20210330
+// density map filter for exponents
+pub fn density_filter<Q, D, G, S>(
+    bases: S,
+    density_map: D,
+    exponents: Arc<Vec<<<G::Engine as ScalarEngine>::Fr as PrimeField>::Repr>>
+) ->  (Arc<Vec<G>>, Arc<Vec<<<G::Engine as ScalarEngine>::Fr as PrimeField>::Repr>>, usize, usize)
+where
+    for<'a> &'a Q: QueryDensity,
+    D: Send + Sync + 'static + Clone + AsRef<Q>,
+    G: CurveAffine,
+    G::Engine: crate::bls::Engine,
+    S: SourceBuilder<G>,
+{
+    let mut exps = vec![exponents[0]; exponents.len()];
+    let mut n = 0;
+    for (&e, d) in exponents.iter().zip(density_map.as_ref().iter()) {
+        if d {
+            exps[n] = e;
+            n += 1;
+        }
+    }
+    let (bss, skip) = bases.get();
+    (bss,Arc::new(exps),skip,n)
+}
+
 /// Perform multi-exponentiation. The caller is responsible for ensuring the
 /// query size is the same as the number of exponents.
 pub fn multiexp<Q, D, G, S>(
@@ -311,7 +337,9 @@ where
             }
 
             let (bss, skip) = bases.clone().get();
-            k.multiexp(pool, bss, Arc::new(exps.clone()), skip, n)
+            // Modified by jackoelv for C2 20210330
+            // k.multiexp(pool, bss, Arc::new(exps.clone()), skip, n)
+            k.multiexp(pool, bss, Arc::new(exps), skip, n)
         }) {
             return Waiter::done(Ok(p));
         }
@@ -340,6 +368,59 @@ where
     }
     #[cfg(not(feature = "gpu"))]
     result
+}
+
+// Added by jackoelv for C2 20210330
+// fulldensity
+pub fn multiexp_fulldensity<Q, D, G, S>(
+    pool: &Worker,
+    bases: S,
+    _density_map: D,
+    exponents: Arc<Vec<<<G::Engine as ScalarEngine>::Fr as PrimeField>::Repr>>,
+    kern: &mut Option<gpu::LockedMultiexpKernel<G::Engine>>,
+) -> Waiter<Result<<G as CurveAffine>::Projective, SynthesisError>>
+where
+    for<'a> &'a Q: QueryDensity,
+    D: Send + Sync + 'static + Clone + AsRef<Q>,
+    G: CurveAffine,
+    G::Engine: crate::bls::Engine,
+    S: SourceBuilder<G>,
+{
+    if let Some(ref mut kern) = kern {
+        if let Ok(p) = kern.with(|k: &mut gpu::MultiexpKernel<G::Engine>| {
+            let (bss, skip) = bases.clone().get();
+            k.multiexp(pool, bss, exponents.clone(), skip, exponents.len())
+        }) {
+            let result = Waiter::done(Ok(p));
+            return result
+        }
+    }
+    Waiter::done(Err(SynthesisError::GPUError(gpu::GPUError::GPUDisabled)))
+}
+
+// Added by jackoelv for C2 20210330
+// skipdensity
+pub fn multiexp_skipdensity<G>(
+    pool: &Worker,
+    bss: Arc<Vec<G>>,
+    exps: Arc<Vec<<<G::Engine as ScalarEngine>::Fr as PrimeField>::Repr>>,
+    skip: usize,
+    n: usize,
+    kern: &mut Option<gpu::LockedMultiexpKernel<G::Engine>>,
+) -> Waiter<Result<<G as CurveAffine>::Projective, SynthesisError>>
+where
+    G: CurveAffine,
+    G::Engine: crate::bls::Engine,
+{
+    if let Some(ref mut kern) = kern {
+        if let Ok(p) = kern.with(|k: &mut gpu::MultiexpKernel<G::Engine>| {
+            k.multiexp(pool, bss.clone(), exps.clone(), skip, n)
+        }) {
+            let result = Waiter::done(Ok(p));
+            return result
+        }
+    }
+    Waiter::done(Err(SynthesisError::GPUError(gpu::GPUError::GPUDisabled)))
 }
 
 #[cfg(any(feature = "pairing", feature = "blst"))]
